1f97f8e82b4b5f4b91ac154edecddbf9c7d9c9e9
Sergey Intyashev <seintyashev@mail.ru>
2022-08-03 13:45:53 +0400

    Add authenticate user and create access token defs

--- auth-service/app/api/security.py
+++ auth-service/app/api/security.py
@@ -1,5 +1,9 @@
-from passlib.context import CryptContext
+from datetime import timedelta
 
+from passlib.context import CryptContext
+from db_manager import get_user_by_username
+from jose import jwt
+from app.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
@@ -9,3 +13,19 @@ def get_password_hash(password):
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+async def authenticate_user(username, password):
+    user = await get_user_by_username(username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    to_encode.update({"exp": timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
