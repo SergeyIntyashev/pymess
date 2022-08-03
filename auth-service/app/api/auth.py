02f70795e6a5422b443a266a7aa95b2b797def7e
Sergey Intyashev <seintyashev@mail.ru>
2022-08-03 13:46:05 +0400

    Add login endpoint

--- auth-service/app/api/auth.py
+++ auth-service/app/api/auth.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends, HTTPException
+from datetime import timedelta
+
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from models import UserIn, UserInDB
+from models import UserIn, UserInDB, Token
 
 import security
 import db_manager
@@ -20,6 +22,16 @@ async def sign_up(payload: UserIn):
     return await db_manager.add_user(new_user)
 
 
-@auth.post('/login')
+@auth.post('/login', response_model=Token)
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    pass
+    user = await security.authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = security.create_access_token(
+        data={"sub": user.username}
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
