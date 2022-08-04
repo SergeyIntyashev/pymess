57e83665a8366abf22362a1b07dea6fe50c790af
Sergey Intyashev <seintyashev@mail.ru>
2022-08-04 18:50:11 +0400

    Remove email from users. Reimport database uri

--- auth-service/app/api/db.py
+++ auth-service/app/api/db.py
@@ -1,24 +1,19 @@
-import os
 import sqlalchemy.sql.expression
 
 
 from sqlalchemy import (Column, Integer, MetaData, String, Table,
                         create_engine, Boolean)
 from databases import Database
-from dotenv import load_dotenv
-
-DATABASE_URI = f"postgresql://{os.getenv('db_username')}:{os.getenv('db_password')}@db/{os.getenv('db_dev')}"
+from config import DATABASE_URI
 
 engine = create_engine(DATABASE_URI)
 metadata = MetaData()
-load_dotenv()
 
 users = Table(
     'users',
     metadata,
     Column('id', Integer, primary_key=True),
     Column('username', String(50), unique=True, index=True),
-    Column('email', String(100)),
     Column('fullname', String(150)),
     Column('is_active', Boolean, server_default=sqlalchemy.true(), nullable=False),
     Column('hashed_password', String()),
