fb99da0a7efd099b42a6c461beaa48e8f7aa4c2b
seintyashev@mail.ru <Moloko47#>
2022-08-12 09:05:27 +0400

    Collect schemes in one file

--- auth-service/app/schemes/schemes.py
+++ auth-service/app/schemes/schemes.py
@@ -25,3 +25,12 @@ class User(UserSemi):
 
 class UserInDB(User):
     hashed_password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
