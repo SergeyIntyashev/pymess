225978102a5500631538f33b2e898c582aaab0bc
Sergey Intyashev <seintyashev@mail.ru>
2022-08-17 12:24:54 +0400

    Add prefix to routers (#2)

--- messenger-service/app/api/api.py
+++ messenger-service/app/api/api.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from app.api.routers import rooms, users, messages
 
 api_router = APIRouter()
-api_router.include_router(rooms.router)
-api_router.include_router(users.router)
-api_router.include_router(messages.router)
+api_router.include_router(rooms.router, prefix='/rooms')
+api_router.include_router(users.router, prefix='/users')
+api_router.include_router(messages.router, prefix='/messages')
