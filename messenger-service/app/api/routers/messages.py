7422179be210dc0f59617c089cb69cbc7848656e
Sergey Intyashev <seintyashev@mail.ru>
2022-08-18 19:56:46 +0400

    Remove send message endpoint (#2)

--- messenger-service/app/api/routers/messages.py
+++ messenger-service/app/api/routers/messages.py
@@ -8,32 +8,6 @@ from loguru import logger
 router = APIRouter()
 
 
-# TODO move to sender service
-# @router.post(response_model=MessageInDB, status_code=status.HTTP_201_CREATED)
-# async def send_message(request: Request, message: Message,
-#                        messages: MessagesRepository = Depends(),
-#                        users: UsersRepository = Depends(),
-#                        rooms: RoomsRepository = Depends()):
-#     # checkings user
-#     query_user_matching_check(request, message.sender_id)
-#     await user_in_room_check(message.sender_id, message.room_id, rooms)
-#     if message.recipient_id:
-#         await block_user_check(message.recipient_id, message.sender_id, users)
-#
-#     new_message = MessageInDB(
-#         **message.dict(),
-#         id=uuid4()
-#     )
-#     await messages.create(new_message)
-#
-#     # TODO send message to Kafka
-#
-#     logger.info(f"User with id {message.sender_id} send message "
-#                 f"in room with id {message.room_id}")
-#
-#     return new_message
-
-
 @router.delete('/{message_id}', status_code=status.HTTP_200_OK)
 async def delete_message(request: Request, message_id: UUID,
                          messages: MessagesRepository = Depends()):
